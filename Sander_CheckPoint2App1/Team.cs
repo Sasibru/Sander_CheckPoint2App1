ba35210cb240d226a59120e6b369c7d5b59842a4
Sasibru <sander.simonsen.brustad@gmail.com>
2023-11-13 08:53:58 +0100

    Add Team class

---
+++ Sander_CheckPoint2App1/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sander_CheckPoint2App1
+{
+	public abstract class Team
+	{
+		string? Name { get; set; }
+		List<string> Players { get; set;}
+
+        public Team(string? name = null)
+        {
+            Name = name;
+        }
+    }
+}
