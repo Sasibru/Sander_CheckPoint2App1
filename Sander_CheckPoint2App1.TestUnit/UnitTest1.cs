782efc201c4e38caf610e8b341884f1df25a3c6f
Sasibru <sander.simonsen.brustad@gmail.com>
2023-11-13 09:57:18 +0100

    Add TestUnit project

---
+++ Sander_CheckPoint2App1.TestUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sander_CheckPoint2App1.TestUnit
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
--- Sander_CheckPoint2App1/Program.cs
+++ Sander_CheckPoint2App1/Program.cs
@@ -5,11 +5,11 @@
 		static void Main(string[] args)
 		{
 			Team team = new("A Great Team");
-			Player player = new("Shaquille O’Neal", "34");
+			Player aGreatCenter = new("Shaquille O’Neal", "34");
 			Player theNotGoat = new("Kevin Durant", "35");
 			Star theGoat = new("Michael Jordan", "23", "The undisputed GOAT");
 
-			team.AddPlayer(player);
+			team.AddPlayer(aGreatCenter);
 			team.AddPlayer(theNotGoat);
 			team.AddPlayer(theGoat);
 
