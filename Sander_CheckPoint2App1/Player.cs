eb097b8a5cfa632c6c508e2593649d7dbdbae579
Sasibru <sander.simonsen.brustad@gmail.com>
2023-11-13 09:01:59 +0100

    Add Player class

---
+++ Sander_CheckPoint2App1/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sander_CheckPoint2App1
+{
+	public class Player : Team
+	{
+		string? Name {  get; set; }
+		string? Number { get; set; }
+
+        public Player(string? name = null, string? number = null) : base(name)
+        {
+            Name = name;
+            Number = number;
+        }
+
+    }
+}
